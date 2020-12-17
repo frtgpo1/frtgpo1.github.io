d3df1b4ff36efd17351f5badb89c15f6b056926c
frtgpo10 <o.egbers@julius66.com>
2020-12-17 16:22:06 +0100

    deleted files

--- ALL PROJECTS/Confetti/spec/jasmine-jquery-test.js
+++
@@ -1,9 +0,0 @@
-describe('My suite', function () {
-  beforeEach(function () {
-    jasmine.addMatchers(matchers);
-  });
-
-  it('passes if the element has the specified class', function () {
-    expect($('<div class="some-class"></div>')).toHaveClass('some-class');
-  });
-});
