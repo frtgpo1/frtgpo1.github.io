d8603faaf1008ba51bc6eb863f678d53353a1a2a
Olaf <frtgpo2@web.de>
2021-07-20 21:19:08 +0200

    Updates

---
+++ precache-manifest.25c9533b52a8b090d7671fe7e76743dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63066fef4e1298a93143f4f75d3026d7",
+    "url": "/gacgug/index.html"
+  },
+  {
+    "revision": "cd8ee1a45aac35c2a7b8",
+    "url": "/gacgug/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "75ffb21fbfb67aecd734",
+    "url": "/gacgug/static/css/main.e54054b5.chunk.css"
+  },
+  {
+    "revision": "cd8ee1a45aac35c2a7b8",
+    "url": "/gacgug/static/js/2.e67c8a70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gacgug/static/js/2.e67c8a70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ffb21fbfb67aecd734",
+    "url": "/gacgug/static/js/main.1ff3ca40.chunk.js"
+  },
+  {
+    "revision": "db5739a56eafee0a9aaf",
+    "url": "/gacgug/static/js/runtime-main.815b3143.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/gacgug/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frtgpo1.github.io/precache-manifest.8ec71b9d82561c92ea9189f09e1265dc.js"
+  "/gacgug/precache-manifest.25c9533b52a8b090d7671fe7e76743dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frtgpo1.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gacgug/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
